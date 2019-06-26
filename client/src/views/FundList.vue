30664ee1be4e3ad47f9b7f46cb122d04238af37e
ljw <1472406718@qq.com>
2019-06-26 20:32:38 +0800

    分页功能实现

--- client/src/views/FundList.vue
+++ client/src/views/FundList.vue
@@ -90,6 +90,21 @@
       </template>
     </el-table-column>
     </el-table>
+    <el-row>
+      <el-col :span="10">
+        <div class="pagination">
+          <el-pagination
+            @size-change="handleSizeChange"
+            @current-change="handleCurrentChange"
+            :current-page.sync="paginations.page_index"
+            :page-sizes="paginations.page_sizes"
+            :page-size="paginations.page_size"
+            :layout="paginations.layout"
+            :total="paginations.total">
+          </el-pagination>
+        </div>
+      </el-col>
+    </el-row>
     <dia-log :dialog="dialog" @changeShow="changeShow" :formData="formData"></dia-log>
   </div>
 </template>
@@ -104,7 +119,15 @@ export default {
   inject: ["reload"],
   data() {
     return {
+      paginations: {
+        page_index: 1,  // 当前位于哪页
+        total: 0, // 总数
+        page_size: 5, // 当前一页显示多少条
+        page_sizes: [5, 10, 15, 20], // 每页显示多少条
+        layout: "total, sizes, prev, pager, next, jumper" // 翻页属性
+      },
       tableData: [],
+      allTableData: [],
       formData: {
         type: '',
         name: '',
@@ -125,6 +148,30 @@ export default {
     this.getData();
   },
   methods: {
+    handleSizeChange (page_size) {
+      // 切换page_size,选择每页显示多少条数据
+      this.paginations.page_index = 1;
+      this.paginations.page_size = page_size;
+      this.tableData = this.allTableData.filter((item, index) => {
+        return index < page_size
+      })
+    },
+    handleCurrentChange (page) {
+      // 获取当前页page
+      // 获取当前页前面数据总数index
+      let index = this.paginations.page_size*(page - 1);
+      // 数据总数
+      let nums = this.paginations.page_size*page;
+      // 容器
+      let tables = [];
+
+      for (let i = index; i < nums; i++) {
+        if (this.allTableData[i]) {
+          tables.push(this.allTableData[i])
+        }
+        this.tableData = tables
+      }
+    },
     changeShow (show) {
       console.log(show)
       this.dialog.show = show;
@@ -135,11 +182,24 @@ export default {
         .then((res) => {
           console.log(res)
           console.log(res.data)
-          this.tableData = res.data;
+          this.allTableData = res.data;
+          console.log(res.data)
+          // 设置分页功能
+          this.setPaginations();
         })
         .catch((err) => {
           console.log(err)
         })
+    },
+    setPaginations () {
+      // 分页属性设置
+      this.paginations.total = this.allTableData.length
+      this.paginations.page_index = 1
+      this.paginations.page_size = 5
+      // 设置默认分页数据
+      this.tableData = this.allTableData.filter((item, index) => {
+        return index < this.paginations.page_size
+      })
     },
     handleEdit (index, row) {
       // 编辑
@@ -189,5 +249,10 @@ export default {
 }
 .wrapper .btn{
   float: right;
+}
+.pagination {
+  position: absolute;
+  right: 0;
+  margin-top: 15px;
 }
 </style>
