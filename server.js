3fb009f5a51f0bf6604c8db6762ba8d344e97578
ljw <1472406718@qq.com>
2019-09-08 15:13:35 +0800

    项目完结

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use("/api/users", routes);
 app.use('/api/profiles', profiles)
 app.use('/api/info', info)
 app.use('/uploads', express.static(__dirname + '/uploads'))
-// app.use(express.static('./dist'));
+app.use(express.static('./dist'));
 // passport初始化
 app.use(passport.initialize());
 
